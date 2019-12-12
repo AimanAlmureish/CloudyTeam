ce3238484c5b7591a1a2d5ff63c7d1e9a834ae1e
Atheeralaghbari <atheeralaghbari61@gmail.com>
2019-12-13 00:51:35 +0330

    Updates

---
+++ precache-manifest.b4789ef76c3bf91b7e570b9be2531111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c92a33c15d24d9f3c4f9c68844b8e26",
+    "url": "/CloudyTeam/index.html"
+  },
+  {
+    "revision": "d4f983fa62ce31be168d",
+    "url": "/CloudyTeam/static/css/main.7abd5b49.chunk.css"
+  },
+  {
+    "revision": "93dab70bc1c9c8b828a8",
+    "url": "/CloudyTeam/static/js/2.3aad43cf.chunk.js"
+  },
+  {
+    "revision": "d4f983fa62ce31be168d",
+    "url": "/CloudyTeam/static/js/main.bf1fe811.chunk.js"
+  },
+  {
+    "revision": "30454e39a1f7e199c275",
+    "url": "/CloudyTeam/static/js/runtime-main.a7ab4845.js"
+  }
+]);
